6d26f861725680cbc733cecaca2fc1ddc6314c42
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 22:48:04 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+    before_action :set_post, only: [:show, :edit, :update] 
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,11 +12,14 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
+    if @post.valid? 
+      @post.save 
     redirect_to post_path(@post)
+  else 
+    render:new
   end
+end 
 
   private
 
