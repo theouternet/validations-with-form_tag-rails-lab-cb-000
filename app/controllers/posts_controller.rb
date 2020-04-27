3e99ea4c6b51314be87ad2e121737e37e771738d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 22:52:29 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,19 +8,29 @@ class PostsController < ApplicationController
   
   def new 
     @post = Post.new
+end 
+
+def create 
+  @post = Post.new(post_params)
+  if @post.valid?
+    @post.save 
+    redirect_to post_path(@post)
+  else 
+    render :new
+end 
+
 
   def edit
     @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
+    @post.assign_attributes(post_params)
     if @post.valid? 
       @post.save 
     redirect_to post_path(@post)
   else 
-    render:new
+    render:edit
   end
 end 
 
